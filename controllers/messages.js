a707bca948ccd2f0a7172ea837bdfb80a336ec9f
Sam Calamos <sam_calamos@hotmail.com>
2019-06-05 01:02:30 +1000

    adding more endpointss!

--- controllers/messages.js
+++ controllers/messages.js
@@ -12,7 +12,7 @@ exports.allThreads = async (req, res, next) => {
 exports.weeklyAverage = async (req, res, next) => {
     // const query = "SELECT date_part('year', sent_time::date) as year, date_part('week', sent_time::date) as week, count(body) FROM download_messages join download_message_key dmk on download_messages.thread_id = dmk.id where dmk.name = $1 GROUP BY year, week ORDER BY year, week;"
     const query = "SELECT * FROM weekly_messages($1)";
-    const values = [req.params.group_name];
+    const values = [req.params.thread_name];
 
     const dbRes = await pool.query(query, values);
     res.send(dbRes.rows);
@@ -21,8 +21,34 @@ exports.weeklyAverage = async (req, res, next) => {
 exports.monthlyAverage = async (req, res, next) => {
     // const query = "SELECT date_part('year', sent_time::date) as year, date_part('week', sent_time::date) as week, count(body) FROM download_messages join download_message_key dmk on download_messages.thread_id = dmk.id where dmk.name = $1 GROUP BY year, week ORDER BY year, week;"
     const query = "SELECT * FROM monthly_messages($1)";
-    const values = [req.params.group_name];
+    const values = [req.params.thread_name];
 
     const dbRes = await pool.query(query, values);
     res.send(dbRes.rows);
 };
+
+exports.threadContribution  = async (req, res, next) => {
+    const query = "SELECT name, message_count FROM thread_contribution($1)";
+    const values = [req.params.thread_name];
+
+    const dbRes = await pool.query(query, values);
+    res.send(dbRes.rows);
+};
+
+exports.allMessages = async (req, res, next) => {
+    const query = "SELECT body from download_messages join download_message_key dmk on download_messages.thread_id = dmk.id where dmk.name = $1 and body is not null"
+    const values = [req.params.thread_name];
+
+    const dbRes = await pool.query(query, values);
+    res.send(dbRes.rows);
+};
+
+exports.averageMessageLength = async (req, res, next) => {
+    const query = " SELECT * FROM average_message_length($1)";
+    const values = [req.params.thread_name];
+
+    const dbRes = await pool.query(query, values);
+    res.send(dbRes.rows);
+}
+
+
--- routes/messages.js
+++ routes/messages.js
@@ -6,9 +6,15 @@ const MessagesController = require("../controllers/messages");
 /* GET messages listing. */
 router.get('/threads', MessagesController.allThreads);
 
-router.get('/weekly/:group_name', MessagesController.weeklyAverage);
+router.get('/threads/contribution/:thread_name', MessagesController.threadContribution);
 
-router.get('/monthly/:group_name', MessagesController.monthlyAverage);
+router.get('/weekly/:thread_name', MessagesController.weeklyAverage);
+
+router.get('/monthly/:thread_name', MessagesController.monthlyAverage);
+
+router.get('/body/:thread_name', MessagesController.allMessages);
+
+router.get('/avg/length/:thread_name', MessagesController.averageMessageLength);
 
 
 
